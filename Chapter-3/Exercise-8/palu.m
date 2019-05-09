62235e074b82417c2695f60db01e45867168ec0d
Alessio Falai <falai.alessio@gmail.com>
2019-05-09 09:45:38 +0200

    add exercise 8

---
+++ Chapter-3/Exercise-8/palu.m
@@ -0,0 +1,27 @@
+function [A, p] = palu (A)
+%
+% function [A, p] = palu (A) calcola la fattorizzazione LU con pivoting parziale e 
+%                            restituisce la matrice fattorizzata e il vettore
+%                            contentente l'informazione relativa alla matrice 
+%                            di permutazione
+%
+[m, n] = size(A);
+if m ~= n
+    error('Matrice non quadrata!');
+end
+p = 1 : n;
+for i = 1 : n - 1
+    [mi, ki] = max(abs(A(i : n, i)));
+    if mi == 0 
+        error('Matrice singolare!')
+    end
+    ki = ki + i - 1;
+    if ki > i
+        A([i ki], :) = A([ki i], :);
+        p([i ki]) = p([ki i]);
+    end
+    A(i + 1 : n, i) = A(i + 1 : n, i) / A(i, i);
+    A(i + 1 : n, i + 1 : n) = A(i + 1 : n, i + 1 : n) - A(i + 1 : n, i) * A(i, i + 1 : n);
+end
+p = p';
+return
